b831c14b4b7488fd73aff174a26b73e289528c62
Mathis Ouarnier <mathis.ouarnier@liksi.fr>
2023-08-19 14:30:55 +0200

    :rocket: Bump dependencies & set SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
-  nitro: {
-    preset: "netlify",
-  },
+  ssr: true,
   devtools: { enabled: true },
   modules: ["nuxt-svgo"],
   svgo: {
