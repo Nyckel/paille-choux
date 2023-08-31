d4536739792a42cdef6df93b44c2760858301c38
Mathis Ouarnier <mathis.ouarnier@liksi.fr>
2023-08-31 21:10:32 +0200

    :mag: Add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  site: {
+    url: "https://paillechoux.fr",
+  },
   ssr: true,
   devtools: { enabled: true },
-  modules: ["nuxt-svgo", "@nuxt/image"],
+  modules: [
+    "nuxt-svgo",
+    "@nuxt/image",
+    "nuxt-simple-robots",
+    "nuxt-simple-sitemap",
+  ],
   svgo: {
     defaultImport: "component",
   },
